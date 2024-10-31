d6124c0215c57b62eb7ba7b2267e6b8fd45d7852
pranshubasak <pranshubasak@gmail.com>
2024-10-31 18:56:53 +0530

    rebuilding Landingpage

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import LandingHeader from "./_LandingComponents/LandingHeader";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <LandingHeader />
-      {/* <Header />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Faq />
-      <Testimonials />
-      <Download />
-      <Footer /> */}
+
     </main>
   );
 }
