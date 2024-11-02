17366f6722f25674b849a244c0ad5d3f1d424785
pranshubasak <pranshubasak@gmail.com>
2024-11-02 22:19:55 +0530

    Added Icons

--- app/layout.js
+++ app/layout.js
@@ -23,9 +23,32 @@ const poppin = Poppins({
 })
 
 export const metadata = {
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' }
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      }
+    ]
+  },
+  manifest: '/site.webmanifest',
   title: "One Click Video",
   description: "OneClickVideo is an AI-powered video generator designed to simplify the process of creating stunning videos in just one click. Whether you're a content creator, marketer, or small business owner, OneClickVideo provides an intuitive platform where you can transform a simple text prompt into a fully produced video. With support for various art styles, dynamic audio integration, and seamless video stitching, OneClickVideo offers a quick and automated solution to your video creation needs.",
 };
+
+
 
 export default function RootLayout({ children }) {
   return (
